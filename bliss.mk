09812b61bceb59a0a9bbbecfbc213f43413b4eef
Dave Martin <davemartinfsu@gmail.com>
2016-03-14 14:58:47 -0400

    Blissify

--- bliss.mk
+++ bliss.mk
@@ -1,15 +1,15 @@
 # Release name
 PRODUCT_RELEASE_NAME := lt013g
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/lt013g/full_lt013g.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lt013g
-PRODUCT_NAME := cm_lt013g
+PRODUCT_NAME := bliss_lt013g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T311
 PRODUCT_MANUFACTURER := samsung
